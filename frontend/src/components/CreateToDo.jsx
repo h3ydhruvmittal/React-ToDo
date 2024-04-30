db394876db2b61cd409e8564642f695b447f9a86
Dhruv <dhruvdm02@gmail.com>
2024-04-30 20:52:52 +0530

    created frontend

---
+++ frontend/src/components/CreateToDo.jsx
@@ -0,0 +1,12 @@
+export function CreateToDo() {
+  return (
+    <div>
+      <input type="text" placeholder="Title"></input>
+      <br />
+      <input type="text" placeholder="Description"></input>
+      <br />
+
+      <button>Add a ToDo</button>
+    </div>
+  );
+}
